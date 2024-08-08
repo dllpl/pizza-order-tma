8ca1896b5312772f4970ca52dfa92efdf69b5b2b
dllpl <nick.iv.dev@gmail.com>
2024-08-08 11:04:10 +0300

    user-scalable=no

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
+    app: {
+        head: {
+            meta: [
+                { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
+            ]
+        }
+    },
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
