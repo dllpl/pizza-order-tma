d6b776678a61ffe208e86dd8ee6c8e087032f193
dllpl <nick.iv.dev@gmail.com>
2024-08-09 23:08:03 +0300

    Правка

--- stores/tgWebApp.js
+++ stores/tgWebApp.js
@@ -34,7 +34,6 @@ export const useTgWebAppStore = defineStore('tgWebAppStore', {
 
                 this.setGeo()
 
-                useWebAppCloudStorage().removeStorageItems(['initData', 'userData'])
             }
         },
 
